d96007e405a0b97406e38693217445101bd466a3
DevDuhva <167563092+DevDuhva@users.noreply.github.com>
2025-09-25 21:00:39 -0300

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `usuarios` (
+  `ID` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `LOGIN` varchar(50) NOT NULL,
+  `NOME` varchar(150) NOT NULL,
+  `EMAIL` varchar(150) NOT NULL,
+  `SENHA` varchar(80) NOT NULL,
+  `ATIVO` bit(1) DEFAULT b'1'
+);
+
+INSERT INTO `usuarios` (`LOGIN`, `NOME`, `EMAIL`, `SENHA`, `ATIVO`) VALUES 
+('Eduardo', 'Eduardo ataide deo liveira', 'eduardoataidedeoliveira@gmail.com', 'banana', b'1');
